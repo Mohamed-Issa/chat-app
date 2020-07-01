d1530ff28ad1e611fe127eecd85399791483015d
mohamed <mohamed.hamdy.issa@gmail.com>
2020-07-01 15:50:14 +0200

    android & push notification

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,8 +3,22 @@ import 'package:chatapp/widgets/chat/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
